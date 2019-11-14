4d761f0bcaa1a8ee57572427befeef69ee43c43a
Eugenia Kim <elk303@nyu.edu>
2019-11-14 13:25:25 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {...state, friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND":
+            let foundIndex = state.friends.findIndex(friend => friend.id === action.id)
+            let updatedFriends = [...state.friends]
+            updatedFriends.splice(foundIndex, 1)
+            return {friends: updatedFriends}
+        default: 
+            return {...state}
+    }
 }
